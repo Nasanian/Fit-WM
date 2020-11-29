76ff2ae2b5bef926a978c06ffb9cf5ec5825e270
Nasanian <archuseris2@gmail.com>
2020-11-29 03:50:29 -0600

    Changed the way fullscreen functions

--- config.h
+++ config.h
@@ -26,7 +26,7 @@
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
 //static const unsigned int gappx     = 64;
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const unsigned int gappih    = 0;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 0;       /* vert inner gap between windows */
--- dwm.c
+++ dwm.c
@@ -2378,13 +2378,14 @@ Layout *last_layout;
 void
 fullscreen(const Arg *arg)
 {
-	if (selmon->showbar) {
+  selmon->sel->isfullscreen = !selmon->sel->isfullscreen;
+
+	if (!selmon->sel->isfullscreen) {
 		for(last_layout = (Layout *)layouts; last_layout != selmon->lt[selmon->sellt]; last_layout++);
 		setlayout(&((Arg) { .v = &layouts[2] }));
 	} else {
 		setlayout(&((Arg) { .v = last_layout }));
 	}
-	togglebar(arg);
 }
 
 void
